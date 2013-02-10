203c8ec7f47de9076b4b6e351c4f2c82e93bc1eb
Lakshmikant Shrinivas <lakshmikant@gmail.com>
2013-02-10 00:18:32 -0500

    Added jquery uploader plugin. Threw out all the fluff - only the necessary files have been retained.

---
+++ uploader/js/main.js
@@ -0,0 +1,63 @@
+/*
+ * jQuery File Upload Plugin JS Example 7.0
+ * https://github.com/blueimp/jQuery-File-Upload
+ *
+ * Copyright 2010, Sebastian Tschan
+ * https://blueimp.net
+ *
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/MIT
+ */
+
+/*jslint nomen: true, unparam: true, regexp: true */
+/*global $, window, document */
+
+$(function () {
+    'use strict';
+
+    // Initialize the jQuery File Upload widget:
+    $('#fileupload').fileupload({
+        // Uncomment the following to send cross-domain cookies:
+        //xhrFields: {withCredentials: true},
+        url: 'server/php/'
+    });
+
+    // Enable iframe cross-domain access via redirect option:
+    // $('#fileupload').fileupload(
+    //     'option',
+    //     'redirect',
+    //     window.location.href.replace(
+    //         /\/[^\/]*$/,
+    //         '/cors/result.html?%s'
+    //     )
+    // );
+
+ 
+    // Load existing files:
+    $.ajax({
+        // Uncomment the following to send cross-domain cookies:
+        //xhrFields: {withCredentials: true},
+        url: $('#fileupload').fileupload('option', 'url'),
+        dataType: 'json',
+        context: $('#fileupload')[0]
+    }).done(function (result) {
+        $(this).fileupload('option', 'done')
+            .call(this, null, {result: result});
+    });
+    
+    // Initialize the Image Gallery widget:
+    // $('#fileupload .files').imagegallery();
+
+    // Initialize the theme switcher:
+    $('#theme-switcher').change(function () {
+        var theme = $('#theme');
+        theme.prop(
+            'href',
+            theme.prop('href').replace(
+                /[\w\-]+\/jquery-ui.css/,
+                $(this).val() + '/jquery-ui.css'
+            )
+        );
+    });
+
+});
