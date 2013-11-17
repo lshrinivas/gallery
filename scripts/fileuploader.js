d561087749f97dd4c5e5b2f214f333db00cd0adb
lshrinivas <lakshmikant@gmail.com>
2013-11-16 23:36:30 -0500

    Made file uploader process one file at a time, to not overwhelm the browser when there are lots of files.

--- scripts/fileuploader.js
+++ scripts/fileuploader.js
@@ -5,6 +5,11 @@ function FileUploader(options) {
     this.startResize = options.startResize;
     this.endResize = options.endResize;
     this.uploadProgress = options.uploadProgress;
+
+    this.queue = [];
+
+    var boundProcessFile = this.processFile.bind(this);
+    $(this).on('processNext', boundProcessFile);
 }
 
 FileUploader.prototype.filterImages = function(files) {
@@ -25,91 +30,110 @@ FileUploader.prototype.upload = function(albumName, images) {
     var newCanvas = $('<canvas/>');
     newCanvas[0].height = 1000;
     newCanvas[0].width = 1000;
-    var ctx = newCanvas[0].getContext('2d');
+
+    this.canvas = newCanvas[0];
+    this.albumName = albumName;
+
+    for (var i=0; i<images.length; i++) {
+        this.queue.push({ idx: i, image: images[i]});
+    }
+
+    $(this).trigger('processNext');
+}
+
+// resizes and uploads one file from the queue
+FileUploader.prototype.processFile = function() {
+
+    if (!this.queue.length) {
+        return;
+    }
 
     var startResizeCallback = this.startResize;
     var endResizeCallback = this.endResize;
 
-    var self = this;
-    for (var i=0; i<images.length; i++) {
-        var img = new Image();
-        var imgName = images[i].name;
+    var qItem = this.queue.shift();
 
-        // if start resize callback is registered, call it with the current image
-        // index (i.e., i)
-        if (startResizeCallback && (typeof startResizeCallback === 'function')) {
-            startResizeCallback(i);
-        }
+    var img = new Image();
 
-        // need an IIFE to bind local vars, since we're inside a loop
-        EXIF.getData(images[i], (function(imgName, albumName, img, imgIdx) {
-
-            return function() {
-                var orientation = EXIF.getTag(this, "Orientation");
-
-                var reader = new FileReader();
-                reader.onload = function(e) {
-                    img.src = e.target.result
-                }
-                reader.readAsDataURL(this);
-
-                img.onload = function() {
-
-                    var vertical = (img.height > img.width) || (orientation !== 1);
-                    var ratio = Math.min(img.height, img.width) / Math.max(img.height, img.width);
-
-                    // exif orientation of 8 means we need to rotate the image
-                    // anti-clockwise
-                    var rotateLeft = (orientation === 8);
-
-                    // orientation of 6 means we need to rotate clockwise
-                    var rotateRight = (orientation === 6);
-
-                    // the width to draw - this will always be 1000 x (1000 * ratio)
-                    // unless the image has been pre-rotated to be vertical
-                    var drawWidth = 1000;
-                    var drawHeight = 1000 * ratio;
-                    if (vertical) {
-                        newCanvas[0].height = 1000;
-                        newCanvas[0].width = 1000 * ratio;
-
-                        if (rotateLeft) {
-                            ctx.save();
-                            ctx.translate(0, 1000);
-                            ctx.rotate(-Math.PI / 2);
-                        } else if (rotateRight) {
-                            ctx.save();
-                            ctx.translate(newCanvas[0].width, 0);
-                            ctx.rotate(Math.PI / 2);
-                        } else {
-                            // pre-rotated image - set drawWidth and
-                            // drawHeight appropriately
-                            drawWidth = 1000 * ratio;
-                            drawHeight = 1000;
-                        }
-                    } else {
-                        newCanvas[0].height = 1000 * ratio;
-                        newCanvas[0].width = 1000;
-                    }
-
-                    ctx.drawImage(img, 0, 0, drawWidth, drawHeight);
-                    var dataURL = newCanvas[0].toDataURL('image/jpeg', 0.9);
-
-                    if (rotateLeft || rotateRight) {
-                        ctx.restore();
-                    }
-
-                    // call end resize callback with image index
-                    if (endResizeCallback && (typeof endResizeCallback === 'function')) {
-                        endResizeCallback(imgIdx);
-                    }
-
-                    var rawData = dataURLtoBlob(dataURL);
-                    self.sendFile(albumName, imgName, imgIdx, rawData);
-                }
-            }
-        })(imgName, albumName, img, i));
+    // if start resize callback is registered, call it with the current image
+    // index (i.e., i)
+    if (startResizeCallback && (typeof startResizeCallback === 'function')) {
+        startResizeCallback(qItem.idx);
     }
+
+    var newCanvas = this.canvas;
+    var ctx = newCanvas.getContext('2d');
+    var self = this;
+
+    // need an IIFE to bind local vars, since we're inside a loop
+    EXIF.getData(qItem.image, (function(imgName, albumName, img, imgIdx) {
+
+        return function() {
+            var orientation = EXIF.getTag(this, "Orientation");
+
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                img.src = e.target.result
+            }
+            reader.readAsDataURL(this);
+
+            img.onload = function() {
+
+                var vertical = (img.height > img.width) || (orientation !== 1);
+                var ratio = Math.min(img.height, img.width) / Math.max(img.height, img.width);
+
+                // exif orientation of 8 means we need to rotate the image
+                // anti-clockwise
+                var rotateLeft = (orientation === 8);
+
+                // orientation of 6 means we need to rotate clockwise
+                var rotateRight = (orientation === 6);
+
+                // the width to draw - this will always be 1000 x (1000 * ratio)
+                // unless the image has been pre-rotated to be vertical
+                var drawWidth = 1000;
+                var drawHeight = 1000 * ratio;
+                if (vertical) {
+                    newCanvas.height = 1000;
+                    newCanvas.width = 1000 * ratio;
+
+                    if (rotateLeft) {
+                        ctx.save();
+                        ctx.translate(0, 1000);
+                        ctx.rotate(-Math.PI / 2);
+                    } else if (rotateRight) {
+                        ctx.save();
+                        ctx.translate(newCanvas.width, 0);
+                        ctx.rotate(Math.PI / 2);
+                    } else {
+                        // pre-rotated image - set drawWidth and
+                        // drawHeight appropriately
+                        drawWidth = 1000 * ratio;
+                        drawHeight = 1000;
+                    }
+                } else {
+                    newCanvas.height = 1000 * ratio;
+                    newCanvas.width = 1000;
+                }
+
+                ctx.drawImage(img, 0, 0, drawWidth, drawHeight);
+                var dataURL = newCanvas.toDataURL('image/jpeg', 0.9);
+
+                if (rotateLeft || rotateRight) {
+                    ctx.restore();
+                }
+
+                // call end resize callback with image index
+                if (endResizeCallback && (typeof endResizeCallback === 'function')) {
+                    endResizeCallback(imgIdx);
+                }
+
+                var rawData = dataURLtoBlob(dataURL);
+                self.sendFile(albumName, imgName, imgIdx, rawData);
+            }
+        }
+    })(qItem.image.name, this.albumName, img, qItem.idx));
+
 }
 
 FileUploader.prototype.sendFile = function(albumName, imgName, imgIdx, rawData) {
@@ -119,6 +143,8 @@ FileUploader.prototype.sendFile = function(albumName, imgName, imgIdx, rawData) 
     fd.append('file', rawData);
 
     var progressCallback = this.uploadProgress;
+    var self = this;
+
     $.ajax({
         url: this.url,
         type: 'POST',
@@ -143,10 +169,11 @@ FileUploader.prototype.sendFile = function(albumName, imgName, imgIdx, rawData) 
         processData: false
     }).done(function(result) {
         console.log("Result of sending uploaded data is: " + JSON.stringify(result));
-        //console.log("Result of sending uploaded data is: " + result);
     }).fail(function(result, textStatus) {
         console.log("Error uploading file: " + textStatus + ": " + JSON.stringify(result));
-        //console.log("Error uploading file: " + textStatus + ": " + result);
+    }).always(function() {
+        // after file is uploaded (or failed), send message to process next file
+        $(self).trigger('processNext');
     });
 }
 
